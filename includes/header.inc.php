8ced57cb6eb7ca94d6241fa0e7a0fbae624e4832
Kenneth Watson <git@shrimpworks.za.net>
2018-03-25 08:21:14 +0200

    move header into its own file and restyle with css

---
+++ includes/header.inc.php
@@ -0,0 +1,151 @@
+<?php
+
+$twidth = 720;
+$twidthm = $twidth + 160;
+
+$stylefile = "style{$layout}.css";
+$logofile = "utstatsdblogo.png";
+$utlogofile = "utlogo{$layout}.png";
+if (!file_exists("resource/{$utlogofile}")) {
+  $utlogofile = "utlogo.png";
+}
+
+echo <<<EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <title>$title</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="Content-Style-Type" content="text/css" />
+  <link rel="icon" href="resource/uicon.png" type="image/png" />
+  <link rel="stylesheet" href="resource/{$stylefile}" type="text/css" />
+</head>
+
+EOF;
+
+if (!$navbar) {
+  // =========== Side Menu Bar ===========
+  echo <<<EOF
+<body style="background: url(resource/sidebar{$layout}.gif); background-repeat: repeat-y">
+
+<table cellpadding="0" cellspacing="0" border="0"><tr>
+<td width="159" class="sidebar" valign="top">
+  <a href="http://www.unrealtournament.com"><img src="resource/{$utlogofile}" border="0" alt="Unreal Tournament Logo" /></a>
+  <br />
+  <p><a class="sidebar" href="index.php">{$LANG_INC_MAIN}</a></p>
+  <p><a class="sidebar" href="index.php?stats=matches">{$LANG_INC_MATCHES}</a></p>
+  <p><a class="sidebar" href="index.php?stats=players">{$LANG_INC_PLAYERS}</a></p>
+
+EOF;
+
+  if (isset($ranksystem) && $ranksystem) {
+    echo <<<EOF
+  <p><a class="sidebar" href="rankings.php">{$LANG_INC_RANKINGS}</a></p>
+
+EOF;
+  }
+
+  echo <<<EOF
+  <p><a class="sidebar" href="index.php?stats=maps">{$LANG_INC_MAPS}</a></p>
+
+EOF;
+
+  if ($serverlist) {
+    echo <<<EOF
+  <p><a class="sidebar" href="index.php?stats=servers">{$LANG_INC_SERVERS}</a></p>
+
+EOF;
+  }
+
+  echo <<<EOF
+  <p><a class="sidebar" href="totals.php">{$LANG_INC_TOTALS}</a></p>
+  <p><a class="sidebar" href="careerhighs.php">{$LANG_INC_CAREERHIGHS}</a></p>
+  <p><a class="sidebar" href="matchhighs.php">{$LANG_INC_MATCHHIGHS}</a></p>
+  <p><a class="sidebar" href="index.php?stats=help">{$LANG_INC_HELP}</a></p>
+
+EOF;
+
+  for ($i = 0; $i < $menulinks; $i++) {
+    if ($i == 0)
+      echo "  <br />\n";
+    echo "  <p>&nbsp;<a class=\"sidebar\" href=\"{$menu_url[$i]}\">{$menu_descr[$i]}</a></p>\n";
+  }
+
+  echo <<<EOF
+</td>
+<td width="$twidth" valign="top" align="center">
+
+<header class="side-nav">
+  <div class="head-logo">
+    <a href="index.php"><img src="resource/{$logofile}" border="0" alt="UTStatsDB" /></a>
+  </div>
+</header>
+
+EOF;
+} else {
+  // =========== Top Menu Bar ===========
+  echo <<<EOF
+<body>
+
+<header class="top-nav">
+  <div class="side-logo">
+    <a href="http://www.unrealtournament.com"><img src="resource/{$utlogofile}" border="0" alt="Unreal Tournament Logo" /></a>
+  </div>
+  <div class="head-logo">
+    <a href="index.php"><img src="resource/{$logofile}" border="0" alt="UTStatsDB" /></a>
+  </div>
+</header>
+
+<table cellpadding="0" cellspacing="0" border="0" align="center">
+<tr>
+<td class="topbar" valign="top" align="center">
+  <a class="topbar" href="index.php">{$LANG_INC_MAIN}</a>
+  &nbsp;&#8729; &nbsp;<a class="topbar" href="index.php?stats=matches">{$LANG_INC_MATCHES}</a>
+  &nbsp;&#8729; &nbsp;<a class="topbar" href="index.php?stats=players">{$LANG_INC_PLAYERS}</a>
+
+EOF;
+
+  if (isset($ranksystem) && $ranksystem)
+    echo "&nbsp;&#8729; &nbsp;<a class=\"topbar\" href=\"rankings.php\">{$LANG_INC_RANKINGS}</a>\n";
+
+  echo "&nbsp;&#8729; &nbsp;<a class=\"topbar\" href=\"index.php?stats=maps\">{$LANG_INC_MAPS}</a>\n";
+
+  if ($serverlist)
+    echo "&nbsp;&#8729; &nbsp;<a class=\"topbar\" href=\"index.php?stats=servers\">{$LANG_INC_SERVERS}</a>\n";
+
+  echo "&nbsp;&#8729; &nbsp;<a class=\"topbar\" href=\"totals.php\">{$LANG_INC_TOTALS}</a>\n";
+
+  if (isset($ranksystem) && $ranksystem && $serverlist)
+    echo "<br />\n";
+  else
+    echo "&nbsp;| &nbsp;";
+
+  echo <<<EOF
+  <a class="topbar" href="careerhighs.php">{$LANG_INC_CAREERHIGHS}</a>
+  &nbsp;&#8729; &nbsp;<a class="topbar" href="matchhighs.php">{$LANG_INC_MATCHHIGHS}</a>
+  &nbsp;&#8729; &nbsp;<a class="topbar" href="index.php?stats=help">{$LANG_INC_HELP}</a>
+
+EOF;
+
+  if ($menulinks)
+    echo "<br />\n";
+  for ($i = 0; $i < $menulinks; $i++) {
+    if ($i == 0)
+      echo "  <br />\n";
+    echo "  &nbsp;<a class=\"topbar\" href=\"{$menu_url[$i]}\">{$menu_descr[$i]}</a>\n";
+  }
+
+  echo <<<EOF
+</td>
+</tr>
+<tr><td>&nbsp;</td></tr>
+<tr>
+<td width="$twidth" valign="top" align="center">
+
+<br />
+
+EOF;
+}
+
+?>
--- includes/main.inc.php
+++ includes/main.inc.php
@@ -146,172 +146,20 @@ function unhtmlspecialchars($string)
   return $string;
 }
 
-$twidth = 720;
-$twidthm = $twidth + 160;
-
 if (!isset($layout) || !$layout) {
   echo "Configuration error.<br />\n";
   exit;
 }
 
-$stylefile = "style{$layout}.css";
-$logofile = "utstatsdblogo.png";
-$utlogofile = "utlogo{$layout}.png";
-if (!file_exists("resource/{$utlogofile}"))
-  $utlogofile = "utlogo.png";
-
 // Team colors
-$teamcolor = array("Red","Blue","Green","Gold");
-$teamcolorbar = array(2,1,3,4);
-$teamclass = array("redteam","blueteam","greenteam","goldteam");
-$teamchat = array("chatred","chatblue","chatgreen","chatgold");
-$teamscore = array("redteamscore","blueteamscore","greenteamscore","goldteamscore");
+$teamcolor = array("Red", "Blue", "Green", "Gold");
+$teamcolorbar = array(2, 1, 3, 4);
+$teamclass = array("redteam", "blueteam", "greenteam", "goldteam");
+$teamchat = array("chatred", "chatblue", "chatgreen", "chatgold");
+$teamscore = array("redteamscore", "blueteamscore", "greenteamscore", "goldteamscore");
 
 header('Content-Type:text/html; charset=utf-8');
 
-echo <<<EOF
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <title>$title</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <meta http-equiv="Content-Style-Type" content="text/css" />
-  <link rel="icon" href="resource/uicon.png" type="image/png" />
-  <link rel="stylesheet" href="resource/{$stylefile}" type="text/css" />
-</head>
-
-EOF;
-
-if (!$navbar) {
-  // =========== Side Menu Bar ===========
-  echo <<<EOF
-<body style="background: url(resource/sidebar{$layout}.gif); background-repeat: repeat-y">
-
-<table cellpadding="0" cellspacing="0" border="0"><tr>
-<td width="159" class="sidebar" valign="top">
-  <a href="http://www.unrealtournament.com"><img src="resource/{$utlogofile}" border="0" alt="Unreal Tournament Logo" /></a>
-  <br />
-  <p><a class="sidebar" href="index.php">{$LANG_INC_MAIN}</a></p>
-  <p><a class="sidebar" href="index.php?stats=matches">{$LANG_INC_MATCHES}</a></p>
-  <p><a class="sidebar" href="index.php?stats=players">{$LANG_INC_PLAYERS}</a></p>
-
-EOF;
-
-  if (isset($ranksystem) && $ranksystem) {
-    echo <<<EOF
-  <p><a class="sidebar" href="rankings.php">{$LANG_INC_RANKINGS}</a></p>
-
-EOF;
-  }
-
-  echo <<<EOF
-  <p><a class="sidebar" href="index.php?stats=maps">{$LANG_INC_MAPS}</a></p>
-
-EOF;
-
-  if ($serverlist) {
-    echo <<<EOF
-  <p><a class="sidebar" href="index.php?stats=servers">{$LANG_INC_SERVERS}</a></p>
-
-EOF;
-  }
-
-  echo <<<EOF
-  <p><a class="sidebar" href="totals.php">{$LANG_INC_TOTALS}</a></p>
-  <p><a class="sidebar" href="careerhighs.php">{$LANG_INC_CAREERHIGHS}</a></p>
-  <p><a class="sidebar" href="matchhighs.php">{$LANG_INC_MATCHHIGHS}</a></p>
-  <p><a class="sidebar" href="index.php?stats=help">{$LANG_INC_HELP}</a></p>
-
-EOF;
-
-  for ($i = 0; $i < $menulinks; $i++) {
-    if ($i == 0)
-      echo "  <font size=\"1\"><br /></font>\n";
-    echo "  <p>&nbsp;<a class=\"sidebar\" href=\"{$menu_url[$i]}\">{$menu_descr[$i]}</a></p>\n";
-  }
-
-  echo <<<EOF
-</td>
-<td width="$twidth" valign="top" align="center">
-
-<table cellpadding="0" cellspacing="0" border="0" width="100%" align="center">
-  <tr>
-    <td align="center">
-      <a href="index.php"><img src="resource/{$logofile}" border="0" alt="UTStatsDB Logo" /></a>
-    </td>
-  </tr>
-</table>
-<font size="1"><br /></font>
-
-EOF;
-}
-else {
-  // =========== Top Menu Bar ===========
-  echo <<<EOF
-<body>
-
-<table cellpadding="0" cellspacing="0" border="0" align="center">
-  <tr>
-    <td align="center">
-      <a href="http://www.unrealtournament.com"><img src="resource/{$utlogofile}" border="0" alt="Unreal Tournament Logo" /></a>
-    </td>
-    <td align="center">
-      <a href="http://www.utstatsdb.com"><img src="resource/{$logofile}" border="0" alt="UTStatsDB Logo" /></a>
-    </td>
-  </tr>
-  <tr><td>&nbsp;</td></tr>
-</table>
-
-<table cellpadding="0" cellspacing="0" border="0" align="center">
-<tr>
-<td class="topbar" valign="top" align="center">
-  <a class="topbar" href="index.php">{$LANG_INC_MAIN}</a>
-  &nbsp;&#8729; &nbsp;<a class="topbar" href="index.php?stats=matches">{$LANG_INC_MATCHES}</a>
-  &nbsp;&#8729; &nbsp;<a class="topbar" href="index.php?stats=players">{$LANG_INC_PLAYERS}</a>
-
-EOF;
-
-  if (isset($ranksystem) && $ranksystem)
-    echo "&nbsp;&#8729; &nbsp;<a class=\"topbar\" href=\"rankings.php\">{$LANG_INC_RANKINGS}</a>\n";
-
-  echo "&nbsp;&#8729; &nbsp;<a class=\"topbar\" href=\"index.php?stats=maps\">{$LANG_INC_MAPS}</a>\n";
-
-  if ($serverlist)
-    echo "&nbsp;&#8729; &nbsp;<a class=\"topbar\" href=\"index.php?stats=servers\">{$LANG_INC_SERVERS}</a>\n";
-
-  echo "&nbsp;&#8729; &nbsp;<a class=\"topbar\" href=\"totals.php\">{$LANG_INC_TOTALS}</a>\n";
-
-  if (isset($ranksystem) && $ranksystem && $serverlist)
-    echo "<br />\n";
-  else
-    echo "&nbsp;| &nbsp;";
-
-  echo <<<EOF
-  <a class="topbar" href="careerhighs.php">{$LANG_INC_CAREERHIGHS}</a>
-  &nbsp;&#8729; &nbsp;<a class="topbar" href="matchhighs.php">{$LANG_INC_MATCHHIGHS}</a>
-  &nbsp;&#8729; &nbsp;<a class="topbar" href="index.php?stats=help">{$LANG_INC_HELP}</a>
-
-EOF;
-
-  if ($menulinks)
-    echo "<br />\n";
-  for ($i = 0; $i < $menulinks; $i++) {
-    if ($i == 0)
-      echo "  <font size=\"1\"><br /></font>\n";
-    echo "  &nbsp;<a class=\"topbar\" href=\"{$menu_url[$i]}\">{$menu_descr[$i]}</a>\n";
-  }
-
-  echo <<<EOF
-</td>
-</tr>
-<tr><td><font size="1">&nbsp;</font></td></tr>
-<tr>
-<td width="$twidth" valign="top" align="center">
-
-<font size="1"><br /></font>
-
-EOF;
-}
+require('header.inc.php');
 
 ?>
